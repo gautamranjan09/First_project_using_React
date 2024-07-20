f1ff38ba099174afd9567e7e52578addc514d7ed
gautamranjan09 <gautamranjan96@gmail.com>
2024-07-20 22:11:01 +0530

    Learning to  Write Complex JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,12 @@
 function ExpenseItem() {
     return (
         <>
+            <div>15 August 2023</div>
+            <p>Book - <span>$10</span></p>
             <p>Food - $5</p>
             <p>Petrol - $20</p>
             <p>Movie - $10</p>
+            <div>Delhi</div>
         </>
     );
 }
