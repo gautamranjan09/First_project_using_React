a0a805f8179666bf9176b28f09e87260bbcd4d70
gautamranjan09 <gautamranjan96@gmail.com>
2024-07-21 18:55:10 +0530

    Learning to add  JavaScript Inside JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 7, 15).toISOString();
+    const expenseTitle = "Insurance";
+    const expenseAmount = 50;
+    const expenseLocation ="Bangalore";
     return (
         <div className="expense-item">
-            <div>15 August 2023 </div>
-            <p className="expense-item__description"> Book - <span className="expense-item__price">$10</span></p>
+            <div>{expenseDate} </div>
+            <p className="expense-item__description"> {expenseTitle} - <span className="expense-item__price">${expenseAmount}</span></p>
             <p className="expense-item__description">Food - <span className="expense-item__price">$5</span></p>
             <p className="expense-item__description">Petrol - <span className="expense-item__price">$20</span></p>
             <p className="expense-item__description">Movie - <span className="expense-item__price">$10</span></p>
-            <div className="expense-item__location">Delhi</div>
+            <div className="expense-item__location">{expenseLocation}</div>
         </div>
     );
 }
