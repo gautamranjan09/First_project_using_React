8e6185ba45d38df784c6f75dd18078da90bbde35
gautamranjan09 <gautamranjan96@gmail.com>
2024-07-21 19:47:05 +0530

    Learning Props Object & Passing Data to Child Component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
+      
       <h1>Let's get Started</h1>
       <p>I am learning React</p>
-      <ExpenseItem/>
+      
+      <ExpenseItem date={new Date(2023, 7, 15)} title="Insurance" price="50" location="Bangalore" />
+      
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hyderabad"
+      ></ExpenseItem>
+      
+      <ExpenseItem date={new Date(2023, 1, 14)} title="Laptop" price="200" location="Mumbai"/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 7, 15).toISOString();
-    const expenseTitle = "Insurance";
-    const expenseAmount = 50;
-    const expenseLocation ="Bangalore";
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate} </div>
-            <p className="expense-item__description"> {expenseTitle} - <span className="expense-item__price">${expenseAmount}</span></p>
+            <div>{props.date.toISOString()} </div>
+            <p className="expense-item__description"> {props.title} - <span className="expense-item__price">${props.price}</span></p>
             <p className="expense-item__description">Food - <span className="expense-item__price">$5</span></p>
             <p className="expense-item__description">Petrol - <span className="expense-item__price">$20</span></p>
             <p className="expense-item__description">Movie - <span className="expense-item__price">$10</span></p>
-            <div className="expense-item__location">{expenseLocation}</div>
+            <div className="expense-item__location">{props.location}</div>
         </div>
     );
 }
